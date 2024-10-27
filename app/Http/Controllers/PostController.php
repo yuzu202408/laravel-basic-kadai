9ab05045dc784a523a5d5b1d4e99646c6f62ace6
yuzu202408 <akebon1321@gmail.com>
2024-10-28 00:22:51 +0900

    再々変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index() {
         $posts = DB::table('posts')->get();
         return view('posts.index', compact('posts'));
+    }
+
+    public function show($id) {
+        // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
+        $post = Post::find($id);
+        // 変数$postをposts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
     }
 }
