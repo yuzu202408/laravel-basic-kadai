92c5665dd1b5cd75b45200fc601a0e77f634d464
yuzu202408 <akebon1321@gmail.com>
2024-10-27 23:03:46 +0900

    コミット

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('投稿一覧');
 });
+
+Route::get('/posts', [PostController::class, 'index']);
